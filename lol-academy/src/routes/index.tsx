05b20ee251af8d204c376aba83060f84e53707a2
Chris-wav <HristosKalatzis@outlook.com.gr>
2026-04-01 21:12:20 +0300

    one of the last pushes

--- lol-academy/src/routes/index.tsx
+++ lol-academy/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import TopBar from "../components/shared/TopBar";
 import Level from "../components/home/Level";
-import ContinueLesson from "../components/home/ContinueLesson";
 import Categories from "../components/learn/Categories";
 
 export const Route = createFileRoute("/")({
